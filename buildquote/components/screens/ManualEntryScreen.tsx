fcb0f58594789c7d103ede7fe36ec34cd4c3b3a6
buildquoteau-jpg <buildquoteau@gmail.com>
2026-03-16 05:52:38 +0000

    Make onBack optional in ManualEntryScreen

--- buildquote/components/screens/ManualEntryScreen.tsx
+++ buildquote/components/screens/ManualEntryScreen.tsx
@@ -7,7 +7,7 @@ import { getOrCreateDraft } from '@/lib/rfqDraft'
 interface ManualEntryScreenProps {
   items: LineItem[]
   onChange: (items: LineItem[]) => void
-  onBack: () => void
+  onBack?: () => void
   onNext: () => void
   onUploadList: () => void
 }
